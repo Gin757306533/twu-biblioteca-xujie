89fee6e3ede946f7a66c2c211a658adc788b8930
xj18624323263 <18624323263@163.com>
2020-08-03 22:16:35 +0800

    extract all the things except main from BibliotecaAPP to Biblioteca , to make the main function cleaner ^V^
    test rebase

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,57 +1,37 @@
 package com.twu.biblioteca;
 
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 
 public class BibliotecaApp{
-    private Menu<String> menu;
+    private Biblioteca biblioteca;
 
-    private Outputer outputer;
-    private List<Book> bookList;
-    private List<Book> backupOfBookList;
-    private List<Movie> movieList;
 
     public BibliotecaApp() throws ParseException {
-        this.outputer = new Outputer();
-        this.outputer.displayMessage(Message.getWelcomeMessage());
-        this.bookList = this.getBookListBegain();
-        this.backupOfBookList = new ArrayList<Book>();
-        this.backupOfBookList.addAll(this.bookList);
-        this.movieList = this.getMovieListBegain();
-
-
-        ArrayList<String> listOfOptions = new ArrayList<String>();
-        listOfOptions.add("List of Books");
-        listOfOptions.add("Checkout a book");
-        listOfOptions.add("Return a book");
-        listOfOptions.add("View a list of available movies");
-        listOfOptions.add("Quit");
-        this.menu = new Menu<String>(listOfOptions);
+        biblioteca = new Biblioteca();
 
     }
+
 
     public static void main(String[] args) throws ParseException {
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
-        bibliotecaApp.getMenu().displayMenu();
+        Biblioteca biblioteca = bibliotecaApp.getBiblioteca();
+        biblioteca.getMenu().displayMenu();
 
         while(true){
-            int ret = bibliotecaApp.interActWithCustomerOnMenu();
+            int ret = biblioteca.interActWithCustomerOnMenu();
             if (ret == -1){
                 break;
             }else if (ret == 1){
-                bibliotecaApp.getMenu().displayMenu();
+                biblioteca.getMenu().displayMenu();
 
             }else if (ret == 2){
-                bibliotecaApp.getMenu().displayMenu();
+                biblioteca.getMenu().displayMenu();
 
             }else if (ret == 3){
-                bibliotecaApp.getMenu().displayMenu();
+                biblioteca.getMenu().displayMenu();
 
             }else if (ret == 4){
-                bibliotecaApp.getMenu().displayMenu();
+                biblioteca.getMenu().displayMenu();
 
             }
         }
@@ -59,202 +39,7 @@ public class BibliotecaApp{
 
     }
 
-    public int getMenuInput(){
-        int menu_i;
-        while(true)
-        {
-            Scanner sc = new Scanner(System.in);
-            try{
-                menu_i = sc.nextInt();
-                if (menu_i > 0 && menu_i <= this.menu.getlistOfOptions().size())break;
-                else{
-                    this.outputer.displayMessage(Message.getInvalidOptionMessage());
-                    continue;
-                }
-
-            }catch (Exception e){
-                this.outputer.displayMessage(Message.getInvalidOptionMessage());
-
-            }
-        }
-        return menu_i;
+    public Biblioteca getBiblioteca() {
+        return biblioteca;
     }
-
-    public int interActWithCustomerOnMenu() throws ParseException {
-        int menu_i = this.getMenuInput();
-        String option = this.menu.getIndexOption(menu_i);
-        if(option == "List of Books"){
-            this.outputer.displayBookListDetail(this.bookList);
-            return 1;
-        }else if (option == "Checkout a book"){
-            int book_i = this.getCheckOutInput();
-            this.checkoutBook(book_i);
-            return 2;
-        }else if (option == "Return a book"){
-            Book book = this.getBookInfo();
-            this.ReturnBook(book);
-            return 3;
-        }else if (option == "View a list of available movies"){
-            this.outputer.displayMovieListDetail(this.movieList);
-            return 4;
-        }else if (option == "Quit"){
-            return -1;
-        }
-        return 1;
-    }
-
-    public Book getBookInfo() throws ParseException {
-        Book book;
-        while(true)
-        {
-            Scanner sc = new Scanner(System.in);
-            try{
-                this.outputer.displayMessage("Please input the title of the book:");
-                String bookTitle = sc.nextLine().trim();
-                this.outputer.displayMessage("Please input the publish date of the book:");
-                String bookPublishDate = sc.nextLine().trim();
-                int author_i = 0;
-                ArrayList<String> authers = new ArrayList<String>();
-                String author = "";
-                do{
-                    author_i += 1;
-                    this.outputer.displayMessage("Please input the " + author_i + " author of the book:");
-                    authers.add(sc.nextLine().trim());
-                }while(author != "");
-                book = new Book(bookTitle, bookPublishDate, authers);
-                return book;
-
-
-            }catch (Exception e){
-                this.outputer.displayMessage("Something goes wrong when return a book!");
-                continue;
-
-            }
-        }
-    }
-
-
-    public boolean ReturnBook(Book book) throws ParseException {
-
-
-        if(this.backupOfBookList.contains(book) && !this.bookList.contains(book)){
-            this.outputer.displayMessage(Message.getReturnBookSuccessfulMessage());
-            this.bookList.add(book);
-            this.outputer.displayLists(this.bookList);
-            return true;
-        }else if(!this.backupOfBookList.contains(book)){
-            this.outputer.displayMessage(Message.getReturnBookFailedMessage());
-            return false;
-        }else{
-            this.outputer.displayMessage(Message.getReturnBookFailedMessage());
-            return false;
-        }
-    }
-
-
-    public int getCheckOutInput(){
-        int book_i;
-        while(true)
-        {
-            Scanner sc = new Scanner(System.in);
-            try{
-                book_i = sc.nextInt();
-                if (book_i > 0 && book_i <= this.bookList.size())break;
-                else if (book_i != -1){
-                    this.outputer.displayMessage(Message.getCheckoutFailedMessage());
-                    this.outputer.displayMessage("If you want to quit the checkout mode, enter -1");
-                    continue;
-                }else{
-                    return -1;
-                }
-
-            }catch (Exception e){
-                this.outputer.displayMessage(Message.getCheckoutFailedMessage());
-                this.outputer.displayMessage("If you want to quit the checkout mode, enter -1");
-                continue;
-
-            }
-        }
-        return book_i;
-    }
-
-    public boolean checkoutBook(int book_i){
-        if(book_i != -1){
-            this.bookList.remove(book_i-1);
-            this.outputer.displayMessage(Message.getCheckoutSuccessfulMessage());
-            this.outputer.displayLists(this.bookList);
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-
-    public List<Book> getBookListBegain() throws ParseException {
-        List<Book> bookList = new ArrayList<Book>();
-        ArrayList<String> authers = new ArrayList<String>();
-        authers.add("Antoine de Saint-Exupéry");
-        Book book = new Book("Le Petit Prince", "1942-10-01", (ArrayList<String>)authers.clone());
-        bookList.add(book);
-
-        authers.clear();
-        authers.add("Khaled Hosseini");
-        book = new Book("The Kite Runner", "2003-01-01",  (ArrayList<String>)authers.clone());
-        bookList.add(book);
-
-        authers.clear();
-        authers.add("George Orwell");
-        book = new Book("Nineteen Eighty-Four", "1949-01-01",  (ArrayList<String>)authers.clone());
-        bookList.add(book);
-        return bookList;
-    }
-
-    public List<Movie> getMovieListBegain(){
-        List<Movie> movieList = new ArrayList<Movie>();
-        Movie movie = new Movie("he Shawshank Redemption", "1994", "Frank Darabont", 10);
-        movieList.add(movie);
-        movie = new Movie("Léon", "1994", "Luc Besson", 9);
-        movieList.add(movie);
-        movie = new Movie(" Green Book", "2018", "Peter Farrelly", 8);
-        movieList.add(movie);
-        return movieList;
-
-    }
-
-
-    public Menu<String> getMenu() {
-        return menu;
-    }
-
-    public void setMenu(Menu<String> menu) {
-        this.menu = menu;
-    }
-
-    public Outputer getOutputer() {
-        return outputer;
-    }
-
-    public void setOutputer(Outputer outputer) {
-        this.outputer = outputer;
-    }
-
-    public void setBookList(List<Book> bookList) {
-        this.bookList = bookList;
-    }
-
-    public void setMovieList(List<Movie> movieList) {
-        this.movieList = movieList;
-    }
-
-    public List<Book> getBookList() {
-        return bookList;
-    }
-
-    public List<Movie> getMovieList() {
-        return movieList;
-    }
-
-
-
-
 }
---
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -0,0 +1,81 @@
+package com.twu.biblioteca;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import static org.junit.Assert.assertThat;
+
+public class BibliotecaTest {
+    private Biblioteca biblioteca;
+
+    @Rule
+    public final SystemOutRule log = new SystemOutRule().enableLog();
+
+
+    @Before
+    public void init() throws ParseException {
+        this.biblioteca = new Biblioteca();
+    }
+
+    @Test
+    public void testMovieList(){
+        //given
+
+        //when
+        ArrayList<Movie> movieArrayList = (ArrayList<Movie>) biblioteca.getMovieList();
+
+        //then
+        assertThat(movieArrayList.size(), CoreMatchers.is(3));
+    }
+
+    @Test
+    public void testBookList() throws ParseException {
+        //given
+
+        //when
+        ArrayList<Book> bookArrayList = (ArrayList<Book>)this.biblioteca.getBookList();
+
+        //then
+        assertThat(bookArrayList.size(), CoreMatchers.is(3));
+    }
+
+    @Test
+    public void testBookListDetail() throws ParseException {
+        this.biblioteca.getOutputer().displayBookListDetail(this.biblioteca.getBookList());
+    }
+
+    @Test
+    public void testCheckout() throws ParseException {
+        //given
+
+        //when
+        int beforeCount = biblioteca.getBookList().size();
+        biblioteca.checkoutBook(1);
+
+        //then
+        assertThat(beforeCount, CoreMatchers.is(biblioteca.getBookList().size()+1));
+    }
+
+    @Test
+    public void testReturn() throws ParseException {
+        //when
+        ArrayList<String> authers = new ArrayList<String>();
+        authers.add("Antoine de Saint-Exupéry");
+        Book book = new Book("Le Petit Prince", "1942-10-01", (ArrayList<String>)authers.clone());
+
+
+        //given
+        biblioteca.checkoutBook(1);
+        int beforeCount = biblioteca.getBookList().size();
+        biblioteca.ReturnBook(book);
+
+        //then
+        assertThat(beforeCount, CoreMatchers.is(biblioteca.getBookList().size()-1));
+
+    }
+}
