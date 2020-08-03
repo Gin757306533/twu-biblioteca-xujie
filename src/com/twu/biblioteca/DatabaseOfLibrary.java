a4f87b90eead3621b7dbcd1badc439f356bf150d
xj18624323263 <18624323263@163.com>
2020-08-04 00:14:26 +0800

    add movie and book to DataBaseOfLibrary class

--- src/com/twu/biblioteca/DatabaseOfLibrary.java
+++ src/com/twu/biblioteca/DatabaseOfLibrary.java
@@ -1,13 +1,18 @@
 package com.twu.biblioteca;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseOfLibrary {
     private List<Customer> customerList;
+    private List<Movie> movieList;
+    private List<Book> bookList;
 
-    public DatabaseOfLibrary() {
+    public DatabaseOfLibrary() throws ParseException {
         this.customerList = this.createUserInfo();
+        this.movieList = this.getMovieListBegain();
+        this.bookList = this.getBookListBegain();
     }
 
     public List<Customer> createUserInfo(){
@@ -21,7 +26,46 @@ public class DatabaseOfLibrary {
 
     }
 
+    public List<Book> getBookListBegain() throws ParseException {
+        List<Book> bookList = new ArrayList<Book>();
+        ArrayList<String> authers = new ArrayList<String>();
+        authers.add("Antoine de Saint-Exupéry");
+        Book book = new Book("Le Petit Prince", "1942-10-01", (ArrayList<String>)authers.clone());
+        bookList.add(book);
+
+        authers.clear();
+        authers.add("Khaled Hosseini");
+        book = new Book("The Kite Runner", "2003-01-01",  (ArrayList<String>)authers.clone());
+        bookList.add(book);
+
+        authers.clear();
+        authers.add("George Orwell");
+        book = new Book("Nineteen Eighty-Four", "1949-01-01",  (ArrayList<String>)authers.clone());
+        bookList.add(book);
+        return bookList;
+    }
+
+    public List<Movie> getMovieListBegain(){
+        List<Movie> movieList = new ArrayList<Movie>();
+        Movie movie = new Movie("he Shawshank Redemption", "1994", "Frank Darabont", 10);
+        movieList.add(movie);
+        movie = new Movie("Léon", "1994", "Luc Besson", 9);
+        movieList.add(movie);
+        movie = new Movie(" Green Book", "2018", "Peter Farrelly", 8);
+        movieList.add(movie);
+        return movieList;
+
+    }
+
     public List<Customer> getCustomerList() {
         return customerList;
+    }
+
+    public List<Movie> getMovieList() {
+        return movieList;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
     }
 }
--- test/com/twu/biblioteca/DatabaseOfLibraryTest.java
+++ test/com/twu/biblioteca/DatabaseOfLibraryTest.java
@@ -2,21 +2,26 @@ package com.twu.biblioteca;
 import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertThat;
 
 public class DatabaseOfLibraryTest {
     @Test
-    public void testDatabaseOfLibrary(){
+    public void testDatabaseOfLibrary() throws ParseException {
         // given
         DatabaseOfLibrary databaseOfLibrary = new DatabaseOfLibrary();
 
         // when
         ArrayList<Customer> customerArrayList = (ArrayList<Customer>)databaseOfLibrary.getCustomerList();
+        ArrayList<Movie> movieArrayList = (ArrayList<Movie>)databaseOfLibrary.getMovieList();
+        ArrayList<Book> bookArrayList = (ArrayList<Book>)databaseOfLibrary.getBookList();
 
         // then
         assertThat(customerArrayList.size(), CoreMatchers.is(2));
+        assertThat(movieArrayList.size(), CoreMatchers.is(3));
+        assertThat(bookArrayList.size(), CoreMatchers.is(3));
 
     }
 }
